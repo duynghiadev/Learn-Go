90331bca0235180cff61f0c010525a0a29fdff9e
duynghiadev <duynghia22302@gmail.com>
2024-12-22 17:57:08 +0700

    Lập trình REST API với Golang #24: Kiến trúc cho API List Item

--- VietTran/demo/social-todo-list/main.go
+++ VietTran/demo/social-todo-list/main.go
@@ -8,8 +8,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"social-todo-list/common"
-	"social-todo-list/modules/item/model"
 	ginitem "social-todo-list/modules/item/transport/gin"
 )
 
@@ -25,24 +23,12 @@ func main() {
 
 	r := gin.Default()
 
-	// CRUD: Create, Read, Update, Delete
-	// POST /v1/items (create a new item)
-	// GET /v1/items (get all items)
-	// GET /v1/items/:id (get an item by id)
-	// GET /v1/items?status=done (get items by status)
-	// PUT /v1/items/:id (update an item by id) -> replace all fields ❌
-	// PUT /v1/items?status=done (update items by status)
-	// PATCH /v1/items/:id (update an item by id) -> update some fields ❌
-	// PATCH /v1/items?status=done (update items by status)
-	// DELETE /v1/items/:id (delete an item by id)
-	// DELETE /v1/items?status=done (delete items by status)
-
 	v1 := r.Group("/v1")
 	{
 		items := v1.Group("/items")
 		{
 			items.POST("", ginitem.CreateItem(db))
-			items.GET("", ListItem(db))
+			items.GET("", ginitem.ListItem(db))
 			items.GET("/:id", ginitem.GetItem(db))
 			items.PATCH("/:id", ginitem.UpdateItem(db))
 			items.DELETE("/:id", ginitem.DeleteItem(db))
@@ -55,42 +41,4 @@ func main() {
 		})
 	})
 	r.Run(":3000")
-}
-
-func ListItem(db *gorm.DB) func(*gin.Context) {
-	return func(c *gin.Context) {
-		var paging common.Paging
-
-		if err := c.ShouldBind(&paging); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		paging.Process()
-
-		var result []model.TodoItem
-
-		db = db.Where("status <> ?", "Deleted")
-
-		if err := db.Table(model.TodoItem{}.TableName()).
-			Count(&paging.Total).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		if err := db.Order("id desc").
-			Offset((paging.Page - 1) * paging.Limit).
-			Limit(paging.Limit).Find(&result).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, common.NewSuccessResponse(result, paging, nil))
-	}
 }
