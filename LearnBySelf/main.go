d1fe60d875b24b3709f27d5dba80b0dacaf68906
duynghiadev <duynghia22302@gmail.com>
2024-12-26 14:06:56 +0700

    #4

--- LearnBySelf/main.go
+++ LearnBySelf/main.go
@@ -7,6 +7,9 @@ func add(a int, b int) int {
 	return a + b
 }
 
+// Package level variable
+var packageVar = "Hello, package variable"
+
 func calculate(x, y int, a, b float64, name string) {
 	fmt.Printf("Calculation: x: %d, y: %d, a: %.2f, b: %.2f, name: %s\n", x, y, a, b, name)
 }
@@ -33,14 +36,17 @@ func divide2(a, b int) (result int, err error) {
 }
 
 func main() {
+	// add
 	sum := add(1, 2)
 
+	// calculation
 	calculate(1, 2, 3.14, 2.71, "Golang")
 
+	// sum
 	fmt.Println("Sum is:", sum)
 
+	// swap
 	first, second := swap("Hello", "World")
-
 	fmt.Println("Swapped:", first, second)
 
 	// divide
@@ -58,4 +64,22 @@ func main() {
 	} else {
 		fmt.Println("Result2:", result2)
 	}
+
+	// function level variable
+	var functionVar int = 21
+
+	// declaring multiple variables
+	var x, y, z int = 1, 2, 3
+
+	// declaring with inferred types
+	var inferredVar = "Hello, inferred variable"
+
+	// using zero value initialization
+	var uninitializedVar int
+
+	fmt.Println("packageVar:", packageVar)
+	fmt.Println("Function variable:", functionVar)
+	fmt.Println("x, y, z:", x, y, z)
+	fmt.Println("inferredVar:", inferredVar)
+	fmt.Println("uninitializedVar:", uninitializedVar)
 }
