4e1ea126871fdfe4ffd7c88781c83157c7f0764d
duynghiadev <duynghia22302@gmail.com>
2024-12-26 18:07:13 +0700

    #11

--- LearnBySelf/flow-control-statements/defer/main.go
+++ LearnBySelf/flow-control-statements/defer/main.go
@@ -3,19 +3,42 @@ package main
 import "fmt"
 
 func main() {
+	/*
+		start type 1
+	*/
 	// fmt.Println("start")
 	// defer fmt.Println("Deferred 1")
 	// defer fmt.Println("Deferred 2")
 	// defer fmt.Println("Deferred 3")
 	// fmt.Println("end")
+	/*
+		end type 1
+	*/
 
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered from panic:", r)
-		}
-	}()
+	// ---------------------------------------
 
-	fmt.Println("Start")
-	panic("Something went wrong!")
-	fmt.Println("This line will not be executed")
+	/*
+		start type 2
+	*/
+	// defer func() {
+	// 	if r := recover(); r != nil {
+	// 		fmt.Println("Recovered from panic:", r)
+	// 	}
+	// }()
+
+	// fmt.Println("Start")
+	// panic("Something went wrong!")
+	// fmt.Println("This line will not be executed")
+	/*
+		end type 2
+	*/
+
+	// ---------------------------------------
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
