9106407fdd0a22136aa4822df371f04160e44b5a
duynghiadev <duynghia22302@gmail.com>
2024-12-26 13:58:43 +0700

    #3

--- LearnBySelf/main.go
+++ LearnBySelf/main.go
@@ -22,6 +22,16 @@ func divide(a, b int) (int, error) {
 	return a / b, nil
 }
 
+// Example: Named Return Values
+func divide2(a, b int) (result int, err error) {
+	if b == 0 {
+		err = fmt.Errorf("cannot divide by zero")
+		return // Naked return: return 'result' and 'err' variables
+	}
+	result = a / b
+	return // Naked return
+}
+
 func main() {
 	sum := add(1, 2)
 
@@ -33,10 +43,19 @@ func main() {
 
 	fmt.Println("Swapped:", first, second)
 
+	// divide
 	result, error := divide(10, 2)
 	if error != nil {
 		fmt.Println("Error:", error)
 	} else {
 		fmt.Println("Result:", result)
+	}
+
+	// divide2
+	result2, error2 := divide2(10, 5)
+	if error2 != nil {
+		fmt.Println("Error2:", error2)
+	} else {
+		fmt.Println("Result2:", result2)
 	}
 }
