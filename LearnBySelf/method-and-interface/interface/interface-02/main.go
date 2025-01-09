1ec67595815af1baca70756b4666f33fd886bcaf
duynghiadev <duynghia22302@gmail.com>
2025-01-09 16:41:24 +0700

    interface value with nil

--- LearnBySelf/method-and-interface/interface/interface-02/main.go
+++ LearnBySelf/method-and-interface/interface/interface-02/main.go
@@ -5,10 +5,12 @@ import (
 	"math"
 )
 
+// Abser interface
 type Abser interface {
 	Abs() float64
 }
 
+// MyFloat type
 type MyFloat float64
 
 func (f MyFloat) Abs() float64 {
@@ -18,25 +20,77 @@ func (f MyFloat) Abs() float64 {
 	return float64(f)
 }
 
+// Vertex struct
 type Vertex struct {
 	X, Y float64
 }
 
-// Abs implements Abser.
-func (v *Vertex) Abs() float64 { // Use pointer receiver
+// Abs implements Abser for *Vertex
+func (v *Vertex) Abs() float64 {
+	if v == nil {
+		return 0
+	}
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func main() {
+// I interface
+type I interface {
+	M()
+}
+
+// T struct
+type T struct {
+	S string
+}
+
+// M method implements I for *T
+func (t *T) M() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(t.S)
+}
+
+// Example 1: Demonstrating Abser interface
+func example1() {
+	fmt.Println("Example 1: Abser Interface")
 	var a Abser
 	f := MyFloat(-math.Sqrt2)
 	v := Vertex{3, 4}
 
-	a = f  // MyFloat implements Abser
+	a = f // MyFloat implements Abser
+	fmt.Printf("MyFloat: %v\n", a.Abs())
+
 	a = &v // *Vertex implements Abser
+	fmt.Printf("*Vertex: %v\n", a.Abs())
 
-	// Uncommenting the following line will now cause a compilation error:
+	// Uncommenting the following line will cause a compilation error:
 	// a = v // Vertex (not *Vertex) does NOT implement Abser
+}
 
-	fmt.Println(a.Abs()) // This will call the Abs() method of *Vertex
+// Example 2: Demonstrating I interface with nil values
+func example2() {
+	fmt.Println("\nExample 2: I Interface with Nil Values")
+	var i I
+
+	var t *T
+	i = t
+	describe(i)
+	i.M()
+
+	i = &T{"hello"}
+	describe(i)
+	i.M()
+}
+
+// describe function
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+// main function
+func main() {
+	example1()
+	example2()
 }
