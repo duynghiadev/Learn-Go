1194ad03466233a74c87671aa94f3a76f563b5eb
duynghiadev <duynghia22302@gmail.com>
2025-01-13 18:16:06 +0700

    clean project

--- go-to-do-app/go-server/go.mod
+++
@@ -1,9 +0,0 @@
-module go-server
-
-go 1.14
-
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.4.0
-	go.mongodb.org/mongo-driver v1.7.3
-)
--- golang-workshop/todo-service/pkg/usecase/task/service/complite_task.go
+++
@@ -1,7 +0,0 @@
-package service
-
-import "context"
-
-func (s *Service) CompleteTask(ctx context.Context, ID string) error {
-	return s.Repo.CompleteTask(ctx, ID)
-}
