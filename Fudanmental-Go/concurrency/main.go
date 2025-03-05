ba4bd7b56602d06dadded30b83e5af30848ff33e
duynghiadev <duynghia22302@gmail.com>
2025-03-05 14:57:12 +0700

    The Actor Model in GO part 2 - Maybe Game Server?

--- Fudanmental-Go/concurrency/main.go
+++ Fudanmental-Go/concurrency/main.go
@@ -8,8 +8,41 @@ import (
 	"github.com/anthdm/hollywood/actor"
 )
 
+// 1. spawning children
+// 2. poisoning actors (stopping actors)
+// 3. Event stream
+
+type Inventory struct {
+	Bottles int
+}
+
 type Player struct {
-	HP int
+	HP           int
+	inventoryPID *actor.PID
+}
+
+func (i *Inventory) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Started:
+		_ = msg
+		fmt.Println("inventory actor is started (read state from DB)")
+		c.Engine().Subscribe(c.PID())
+	case actor.Stopped:
+		fmt.Println("inventory actor is stopped (save state to DB)")
+	case drinkBottle:
+		fmt.Println("inventory received drink bottle message")
+		i.Bottles -= msg.amount
+	case MyEvent:
+		fmt.Println("just got a string from the event stream in the inventory BTW:", msg.foo)
+	}
+}
+
+func NewInventory(bottles int) actor.Producer {
+	return func() actor.Receiver {
+		return &Inventory{
+			Bottles: bottles,
+		}
+	}
 }
 
 func NewPlayer(hp int) actor.Producer {
@@ -20,7 +53,7 @@ func NewPlayer(hp int) actor.Producer {
 	}
 }
 
-type takeDamage struct {
+type drinkBottle struct {
 	amount int
 }
 
@@ -28,11 +61,19 @@ func (p *Player) Receive(c *actor.Context) {
 	switch msg := c.Message().(type) {
 	case actor.Started:
 		fmt.Println("player actor is started (read state from DB)")
+		p.inventoryPID = c.SpawnChild(NewInventory(1), "inventory")
+		c.Engine().Subscribe(c.PID())
 	case actor.Stopped:
 		fmt.Println("player actor is stopper (save state to DB)")
-	case takeDamage:
-		fmt.Println("Player is taking damage:", msg.amount)
+	case drinkBottle:
+		c.Forward(p.inventoryPID)
+	case MyEvent:
+		fmt.Println("just got a string from the event stream:", msg.foo)
 	}
+}
+
+type MyEvent struct {
+	foo string
 }
 
 func main() {
@@ -42,10 +83,8 @@ func main() {
 	}
 	pid := e.Spawn(NewPlayer(100), "player", actor.WithID("myuserid358"))
 
-	msg := takeDamage{amount: 999}
-	for i := 0; i < 100; i++ {
-		e.Send(pid, msg)
-	}
-
+	e.Send(pid, drinkBottle{amount: 1})
+	time.Sleep(time.Second * 2)
+	e.BroadcastEvent(MyEvent{foo: "yeah buddy"})
 	time.Sleep(time.Second * 2)
 }
