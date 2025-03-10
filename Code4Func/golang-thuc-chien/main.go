918009534db59b00c70e3eae4464ac7c6ede6eb7
duynghiadev <duynghia22302@gmail.com>
2025-03-10 16:41:50 +0700

    struct

--- Code4Func/golang-thuc-chien/main.go
+++ Code4Func/golang-thuc-chien/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type Student struct {
+	firstName string
+	lastName  string
+	email     string
+}
+
 func main() {
-	fmt.Println("Hello, World! Code4Func")
+	s := Student{
+		firstName: "nghia",
+		lastName:  "Le",
+		email:     "abc@gmail.com",
+	}
+
+	fmt.Println(s)
 }
