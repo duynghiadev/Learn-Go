e5a0ac84fe11600160907555b363b80b88b28f9a
duynghiadev <duynghia22302@gmail.com>
2025-03-10 16:55:29 +0700

    oveview knowlegde golang

--- Code4Func/golang-thuc-chien/main.go
+++ Code4Func/golang-thuc-chien/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Student struct {
 	firstName string
 	lastName  string
 	email     string
+}
+
+func (s *Student) Email() string {
+	s.email = "duynghia@gmail.com"
+	return s.email
+}
+
+func g1() {
+	fmt.Println("1")
 }
 
 func main() {
@@ -14,6 +26,30 @@ func main() {
 		lastName:  "Le",
 		email:     "abc@gmail.com",
 	}
-
 	fmt.Println(s)
+
+	valueEmail := s.Email()
+	fmt.Println(valueEmail)
+
+	fmt.Println(s.email)
+
+	// go routine
+	go g1()
+
+	go func() {
+		fmt.Println("2")
+	}()
+
+	// channel: propose of channel is interactive with go routine
+	c := make(chan int)
+	go func() {
+		c <- 100
+	}()
+
+	go func() {
+		fmt.Println(<-c)
+	}()
+
+	time.Sleep(time.Second)
+
 }
