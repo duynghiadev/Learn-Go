4b0bd772361a1e0c715e0a4b054d973227067b59
duynghiadev <duynghia22302@gmail.com>
2025-03-20 15:32:48 +0700

    Update API documentation to change host from localhost:3000 to localhost:3002 in swagger files

--- Code4Func/backend-github-trending/docs/docs.go
+++ Code4Func/backend-github-trending/docs/docs.go
@@ -294,7 +294,7 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "localhost:3000",
+	Host:        "localhost:3002",
 	BasePath:    "/",
 	Schemes:     []string{},
 	Title:       "Github Trending API",
