04df435d2a212ec82fc7abe7242433dd6ee42c28
duynghiadev <duynghia22302@gmail.com>
2025-03-20 15:34:59 +0700

    Remove deprecated files and clean up project structure; delete API documentation, database configuration, main application logic, handlers, models, and repository implementations to streamline the codebase.

--- Code4Func/backend-github-trending-1/model/req/req_signup.go
+++
@@ -1,7 +0,0 @@
-package req
-
-type ReqSignUp struct {
-	FullName string `json:"fullName,omitempty" validate:"required"` // tags
-	Email    string `json:"email,omitempty" validate:"required"`
-	Password string `json:"password,omitempty" validate:"required"`
-}
--- Code4Func/backend-github-trending-1/model/req/req_user.go
+++
@@ -1,6 +0,0 @@
-package req
-
-type ReqUpdateUser struct {
-	FullName string `json:"fullName,omitempty" validate:"required"` // tags
-	Email    string `json:"email,omitempty" validate:"required"`
-}
